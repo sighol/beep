1d4fc6bed666d590025f94aa4d811681c19ff3cf
Sigurd Holsen <sigurd.holsen@cognite.com>
2021-01-05 14:04:36 +0100

    New improved tone

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ fn main() {
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
 
     // Load a sound from a file, using a path relative to Cargo.toml
-    let bytes = include_bytes!("Beep-09.ogg");
+    let bytes = include_bytes!("tone.ogg");
     let cursor = Cursor::new(bytes.iter());
     let source = rodio::Decoder::new(cursor).unwrap();
     stream_handle.play_raw(source.convert_samples()).unwrap();
 
-    let dt = time::Duration::from_millis(500);
+    let dt = time::Duration::from_millis(1471);
     thread::sleep(dt);
 }
