b0a07e00401e186375e86ec12ca6fff59cf39f3a
Sigurd Holsen <sigurd.holsen@cognite.com>
2021-01-05 16:27:35 +0100

    random

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use rodio::Source;
-use std::io::Cursor;
-use std::{thread, time};
+use std::time;
+use rand::prelude::*;
 
 fn main() {
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
 
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let bytes = include_bytes!("tone.ogg");
-    let cursor = Cursor::new(bytes.iter());
-    let source = rodio::Decoder::new(cursor).unwrap();
-    stream_handle.play_raw(source.convert_samples()).unwrap();
+    let sink = rodio::Sink::try_new(&stream_handle).unwrap();
 
-    let dt = time::Duration::from_millis(1471);
-    thread::sleep(dt);
+    let mut rng = rand::thread_rng();
+
+    for i in 0..10 {
+        let freq: f64 = rng.gen();
+        let freq = 200 + ((freq * 1000.0) as u32);
+        add_sound(&sink, freq, 120);
+    }
+    
+    sink.set_volume(0.5);
+    sink.sleep_until_end();
+}
+
+fn add_sound(sink: &rodio::Sink, freq: u32, millis: u64) {
+    let source = rodio::source::SineWave::new(freq);
+    let source = source.take_duration(time::Duration::from_millis(millis));
+    sink.append(source);
 }
